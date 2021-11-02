ee0aec0330a6be61e222ee095cd61df5bf18ed33
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-11-02 23:14:46 +0300

    [auth.py] get_auth_token & write_private_data for further connections

---
+++ src/auth.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+from data import write_private_data
+from random import randint
+import requests
+
+
+def generate_nonce(length=8):
+    """Generate pseudorandom number."""
+    return ''.join([str(randint(0, 9)) for i in range(length)])
+
+
+def get_user_id(token, c_id):
+    """ get user id by access token. """
+    url = "https://api.twitch.tv/helix/users"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Client-Id": c_id
+    }
+    try:
+        r = requests.get(url, headers=headers)
+    except Exception as err:
+        print(err)
+    return(r.json()['data'][0]['id'])
+
+
+def get_auth_token():
+    """
+    'https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#oauth-implicit-code-flow'
+    """
+    client_id = "dadsrpg93f0tvvq8zhbno69m2e3spr"  # this application client id
+    redirect_uri = "https://localhost"
+    scope = "user:read:follows"
+    state = generate_nonce()  # against simple CSRF attacks
+    url = "".join((
+        "https://id.twitch.tv/oauth2/authorize"
+        f"?client_id={client_id}"
+        f"&redirect_uri={redirect_uri}"
+        "&response_type=token"
+        f"&scope={scope}"
+        f"&state={state}"
+    ))
+    try:
+        r = requests.get(url)
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    if state in r.url:  # for safety check that 'state' is substring in response url
+        print("1) Open following url in your browser.")
+        print("2) If asked to login into twitch, you are required to do so, in order to get 'access_token' only known by twitch, and now also known by YOU! B)")
+        print("3) After successful login, page is not existing! ALL WORK AS EXPECTED!")
+        print("4) Copy from url 'access_token' content (from '=' to first '&' excluding those symbols!) and paste that as input here.")
+        print(f"'{r.url}'")
+        access_token = input("access_token=")
+        # try to get user_id by new access_token & validate that user put right access_token
+        user_id = get_user_id(access_token, client_id)
+        # write to file for using in further connections
+        write_private_data(user_id, access_token, client_id)
+        print("SUCCESS")
+    else:
+        print(f"original state: '{state}' not matches state in response!")
+        print("^ because of that - to prevent possible 'CSRF attack' on you, application was stopped, so nobody could harm you!")
+        exit(666)
