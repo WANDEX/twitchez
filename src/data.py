94e04a01b656793dc8c52ae650752e1bc8d93953
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-11-02 22:59:36 +0300

    [data.py] added write_private_data()
    Write private data to file for using in further connections.
    Also set r+w file permissions to owner only.

--- src/data.py
+++ src/data.py
@@ -7,6 +7,21 @@ import json
 import utils
 
 
+def write_private_data(user_id, access_token, client_id):
+    """ Write private data to file for using in further connections.
+        Also set r+w file permissions to owner only.
+    """
+    file_path = utils.project_root(".private")
+    data = {
+        "u_id": user_id,
+        "token": access_token,
+        "c_id": client_id
+    }
+    with open(file_path, "w") as file:
+        json.dump(data, file, indent=4)
+    file_path.chmod(0o600)
+
+
 def get_private_data(key):
     """ get value by the key from .private file. """
     file_path = utils.project_root(".private")
