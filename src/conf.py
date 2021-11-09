a2872255001c934d545a554ae0c8c6cd1b5f95a3
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-11-09 20:34:19 +0300

    [conf.py] get key() from keys.conf

--- src/conf.py
+++ src/conf.py
@@ -7,11 +7,23 @@ import utils
 glob_conf = utils.project_root("config", "default.conf")
 user_conf = utils.project_root(utils.get_user_conf_dir(), "config.conf")
 
+glob_keys = utils.project_root("config", "keys.conf")
+user_keys = utils.project_root(utils.get_user_conf_dir(), "keys.conf")
+
 config = ConfigParser()
 config.read(glob_conf)
 config.read(user_conf)  # user config takes precedence over global default config
 
+keymap = ConfigParser()
+keymap.read(glob_keys)
+keymap.read(user_keys)  # user config takes precedence over global default config
+
 
 def setting(keyname):
     found = config.get("GENERAL", keyname)
+    return found
+
+
+def key(keyname):
+    found = keymap.get("GENERAL", keyname)
     return found
