b83d275c7bcc7f52a6247bbe84c46aeb4dd175c9
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-11-12 16:16:58 +0300

    [tfl.py] support update time interval
    update_data() - check last update time of json_data & update,
    else -> reuse previously downloaded thumbnails, and json_data.
    
    use id as a key of create_streams_dict() instead of user_name.
    
    if user_name is empty (rare, but those exist!) -> use user_login

--- src/tfl.py
+++ src/tfl.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-from os import listdir
+from os import listdir, sep
 from os.path import basename, splitext
+from pathlib import Path
 import data
 import render
 import thumbnails
@@ -13,50 +14,53 @@ import curses
 # TODO: get HEADER_H value from some Page class or smth like that. (to be able to re-use in all tabs/pages)
 HEADER_H = 5
 
-# TODO: remove all hardcoded DEBUG & if DEBUG statements -> after update methods are finished & implemented
-DEBUG = 1
 
-
-def auto_update():
-    """auto update json data."""
-    # TODO: move this method into data.py
-    # TODO: check last update time of json_data file if > ??? min -> auto_update_data()
-    if DEBUG == 0:
+def update_data() -> dict:
+    """Update json data & return thumbnail paths."""
+    if data.time_to_update_live_streams():
         data.update_live_streams()
-
-
-def update_thumbnails() -> dict:
-    # TODO: check last update time of thumbnails dir if > 5 min -> auto_update_data() & update_thumbnails()
-    if DEBUG == 1:
+        json_data = data.read_live_streams()
+        ids = data.get_entries(json_data, 'id')
+        thumbnail_urls_raw = data.get_entries(json_data, 'thumbnail_url')
+        thumbnail_paths = thumbnails.get_thumbnails(ids, thumbnail_urls_raw)
+    else:
         # do not download thumbnails, use previously downloaded thumbnails
+        json_data = data.read_live_streams()
+        ids = data.get_entries(json_data, 'id')
         thumbnail_dir = utils.get_tmp_dir("thumbnails_live")
-        thumbnail_list = utils.insert_to_all(listdir(thumbnail_dir), thumbnail_dir, opt_sep="/")
+
+        tnames = utils.replace_pattern_in_all(listdir(thumbnail_dir), ".jpg", "")
+        differ = list(set(tnames).difference(set(ids)))
+        fnames = utils.add_str_to_list(differ, ".jpg")  # add file extension back
+        for fname in fnames:
+            # remove thumbnail files of users who is not live streaming now.
+            Path(thumbnail_dir, fname).unlink(missing_ok=True)
+
+        thumbnail_list = utils.insert_to_all(listdir(thumbnail_dir), thumbnail_dir, opt_sep=sep)
         thumbnail_paths = {}
         for path in thumbnail_list:
-            user = basename(splitext(path)[0])  # file basename without .ext
-            thumbnail_paths[user] = path
-    else:
-        json_data = data.read_live_streams()
-        user_list = data.get_entries(json_data, 'user_name')
-        thumbnail_urls_raw = data.get_entries(json_data, 'thumbnail_url')
-        thumbnail_paths = thumbnails.get_thumbnails(user_list, thumbnail_urls_raw)
+            id = basename(splitext(path)[0])  # file basename without .ext
+            thumbnail_paths[id] = path
     return thumbnail_paths
 
 
 def prepare_objects():
     """Prepare objects for thumbnails and boxes."""
+    thumbnail_paths = update_data()
     json_data = data.read_live_streams()
-    user_list = data.get_entries(json_data, 'user_name')
     fls = data.create_streams_dict(json_data)  # dict with user name as key
+    ids = list(fls.keys())
     boxes = render.Boxes()
     grid = render.Grid()
-    grid.key_list = user_list
+    grid.key_list = ids
     gcords = grid.coordinates()
-    thumbnail_paths = update_thumbnails()
-    for user, (x, y) in gcords.items():
-        box = render.Box(user, fls[user]["title"], fls[user]["game_name"], x, y)
-        box.img_path = thumbnail_paths[user]
-        thmb = thumbnails.Thumbnail(user, thumbnail_paths[user], x, y + HEADER_H).ue_params
+    for id, (x, y) in gcords.items():
+        user = fls[id]["user_name"]
+        if not user:  # if user_name is empty (rare, but those exist!)
+            user = fls[id]["user_login"]
+        box = render.Box(user, fls[id]["title"], fls[id]["game_name"], x, y)
+        box.img_path = thumbnail_paths[id]
+        thmb = thumbnails.Thumbnail(id, thumbnail_paths[id], x, y + HEADER_H).ue_params
         boxes.add(box)
         boxes.add_thmb(thmb)
 
@@ -84,7 +88,6 @@ def draw_body(parent):
 
 def following_live_page(stdscr):
     """Main page method."""
-    auto_update()
     prepare_objects()
     draw_header(stdscr)
     draw_body(stdscr)
