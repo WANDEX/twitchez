7a0107a691613a316ee2e6f362f4f9b0ec1010e4
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-11-29 20:15:35 +0300

    [following_live_page.py] page of following live channels

--- src/following_live_page.py
+++ src/following_live_page.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-from os import listdir, sep
-from os.path import basename, splitext
-from pathlib import Path
+from json.decoder import JSONDecoder
 from requests import get
 import data
 import keys
 import render
-import thumbnails
-import utils
+import pages
 
-HEADER_H = render.Tabs.HEADER_HEIGHT
-
-PAGE_NAME = "followed_live_streams"
-CACHE_FILE_NAME = f"{PAGE_NAME}.json"
+PAGE_NAME = "Following Live"
 
 
-def get_followed_live_streams():
+def get_json_data() -> JSONDecoder:
     """requests data from twitch API and return json."""
     u_id = data.get_private_data("u_id")    # user_id
     token = data.get_private_data("token")  # auth token
@@ -31,84 +25,9 @@ def get_followed_live_streams():
     return r.json()
 
 
-def cache_path_live_streams():
-    return data.cache_file_path(CACHE_FILE_NAME)
-
-
-def update_live_streams():
-    return data.update_cache(CACHE_FILE_NAME, get_followed_live_streams())
-
-
-def read_live_streams():
-    return data.read_cache(CACHE_FILE_NAME)
-
-
-def time_to_update_live_streams():
-    """Return True if path mtime > 5 mins from now.
-    (default twitch API update time).
-    """
-    fnf = not Path(cache_path_live_streams()).is_file()
-    if fnf or utils.secs_since_mtime(cache_path_live_streams()) > 300:
-        return True
-    else:
-        return False
-
-
-def update_data() -> dict:
-    """Update json data & return thumbnail paths."""
-    if time_to_update_live_streams():
-        update_live_streams()
-        json_data = read_live_streams()
-        ids = data.get_entries(json_data, 'id')
-        thumbnail_urls_raw = data.get_entries(json_data, 'thumbnail_url')
-        thumbnail_paths = thumbnails.get_thumbnails(ids, thumbnail_urls_raw)
-    else:
-        # do not download thumbnails, use previously downloaded thumbnails
-        json_data = read_live_streams()
-        ids = data.get_entries(json_data, 'id')
-        thumbnail_dir = utils.get_tmp_dir("thumbnails_live")
-
-        tnames = utils.replace_pattern_in_all(listdir(thumbnail_dir), ".jpg", "")
-        differ = list(set(tnames).difference(set(ids)))
-        fnames = utils.add_str_to_list(differ, ".jpg")  # add file extension back
-        for fname in fnames:
-            # remove thumbnail files of users who is not live streaming now.
-            Path(thumbnail_dir, fname).unlink(missing_ok=True)
-
-        thumbnail_list = utils.insert_to_all(listdir(thumbnail_dir), thumbnail_dir, opt_sep=sep)
-        thumbnail_paths = {}
-        for path in thumbnail_list:
-            id = basename(splitext(path)[0])  # file basename without .ext
-            thumbnail_paths[id] = path
-    return thumbnail_paths
-
-
-def grid_func(parent):
-    """return grid for prepared objects of thumbnails and boxes."""
-    thumbnail_paths = update_data()
-    json_data = read_live_streams()
-    fls = data.create_streams_dict(json_data)  # dict with user name as key
-    ids = list(fls.keys())
-    boxes = render.Boxes()
-    grid = render.Grid(parent, PAGE_NAME)
-    grid.key_list = ids
-    gcords = grid.coordinates()
-    for id, (x, y) in gcords.items():
-        user_login = fls[id]["user_login"]  # for composing stream url
-        user_name = fls[id]["user_name"]
-        if not user_name:  # if user_name is empty (rare, but those exist!)
-            user_name = user_login
-        box = render.Box(user_login, user_name, fls[id]["title"], fls[id]["game_name"], x, y)
-        box.img_path = thumbnail_paths[id]
-        box.viewers = str(fls[id]["viewer_count"])
-        thmb = thumbnails.Thumbnail(id, thumbnail_paths[id], x, y + HEADER_H).ue_params
-        boxes.add(box)
-        boxes.add_thmb(thmb)
-    return grid
-
-
 def loop(stdscr):
-    page_class = render.Page(stdscr, PAGE_NAME, grid_func)
+    p = pages.Pages(PAGE_NAME, get_json_data())
+    page_class = render.Page(stdscr, p.page_name, p.grid_func)
     keys.loop(page_class)
 
 
