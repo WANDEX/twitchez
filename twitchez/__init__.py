95d8eaa2a1b1f5fb3f733b11644e65776fbc93af
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-01-19 18:33:52 +0300

    [__init__.py] added: version, license, description, urls, author

---
+++ twitchez/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+__version__ = "0.0.1"
+__license__ = "GPLv3"
+__description__ = "TUI client for twitch with thumbnails"
+__url_repository__ = "https://github.com/WANDEX/twitchez"
+__url_bug_reports__ = "https://github.com/WANDEX/twitchez/issues"
+__url_project__ = __url_repository__
+__author__ = "WANDEX"
