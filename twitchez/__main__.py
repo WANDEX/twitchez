454c3c933916965543f672fc96e9d6a5a0613109
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-01-20 19:48:44 +0300

    [__main__.py] check_auth_data() before launch of the main program,
    get_auth_token() if .private file is empty

---
+++ twitchez/__main__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+def check_auth_data():
+    """Check user auth token before launch of the main program."""
+    from twitchez import fs
+    private_file = fs.private_data_path()
+    # private_file is empty -> get auth token & write to private_file
+    if not private_file.stat().st_size:
+        from twitchez import auth_user
+        auth_user.get_auth_token()
+        print("Launch me again!")
+        exit(69)
+    # TODO: also check that the auth token hasn't expired and is still working
+
+
+def main():
+    check_auth_data()
+    from twitchez import init
+    init.main()
+
+
+if __name__ == "__main__":
+    main()
