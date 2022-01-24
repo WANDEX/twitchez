daefcc24f97b6cee6afe534c94d2ff6bcd713ee6
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-01-24 15:22:17 +0300

    [conf.py] read config files with omitted section.

--- twitchez/conf.py
+++ twitchez/conf.py
@@ -5,6 +5,20 @@ from configparser import ConfigParser
 from pathlib import Path
 from twitchez import TWITCHEZDIR, fs
 
+
+def read_conf(*configs):
+    """Read config files with omitted section.
+    Last config takes precedence over previous configs.
+    """
+    parser = ConfigParser()
+    section = "[GENERAL]\n"
+    for f in configs:
+        if Path(f).is_file():  # check that file exist
+            with open(f) as stream:
+                parser.read_string(section + stream.read())
+    return parser
+
+
 glob_conf_dir = Path(TWITCHEZDIR, "config").resolve()
 
 glob_conf = Path(glob_conf_dir, "default.conf").resolve()
@@ -13,16 +27,10 @@ user_conf = Path(fs.get_user_conf_dir(), "config.conf").resolve()
 glob_keys = Path(glob_conf_dir, "defkeys.conf").resolve()
 user_keys = Path(fs.get_user_conf_dir(), "keys.conf").resolve()
 
-config = ConfigParser()
-config.read(glob_conf)
-config.read(user_conf)  # user config takes precedence over global default config
-
-keymap = ConfigParser()
-keymap.read(glob_keys)
-keymap.read(user_keys)  # user config takes precedence over global default config
-
 temp_vars = Path(fs.get_tmp_dir(), "vars").resolve()
-temp = ConfigParser()
+
+config = read_conf(glob_conf, user_conf)
+keymap = read_conf(glob_keys, user_keys)
 
 
 def setting(keyname):
@@ -37,6 +45,7 @@ def key(keyname):
 
 def tmp_set(option, value, section="GENERAL"):
     """Set tmp variable value."""
+    temp = ConfigParser()
     temp.read(temp_vars)
     if not temp.has_section(section):
         temp.add_section(section)
@@ -47,6 +56,7 @@ def tmp_set(option, value, section="GENERAL"):
 
 def tmp_get(keyname, fallback, section="GENERAL"):
     """Get tmp variable value."""
+    temp = ConfigParser()
     temp.read(temp_vars)
     if not temp.has_section(section):
         temp.add_section(section)
