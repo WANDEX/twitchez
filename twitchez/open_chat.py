d3962bea9ec1ef2ad62080c7e459663828416e0e
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-02-04 16:10:24 +0300

    [open_chat.py] support custom cmd - open_chat_cmd

--- twitchez/open_chat.py
+++ twitchez/open_chat.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
+from twitchez import command
+from twitchez import conf
+import subprocess
 try:
     import webbrowser
 except ImportError:
@@ -10,10 +13,24 @@ else:
     import os
 
 
+chat_cmd = conf.setting("open_chat_cmd")
+executable = command.first_cmd_word(chat_cmd)
+without_funcs = command.without_funcs(executable)
+cmd_check = command.cmd_check(executable)
+
+
 def open_chat(channel_login):
     """Open twitch chat of the channel."""
     url = f"https://www.twitch.tv/popout/{channel_login}/chat?popout="
-    if has_webbrowser:
+    if cmd_check:
+        cmd = chat_cmd.split()
+        cmd.append(url)
+        sub = subprocess.Popen
+        sub(cmd,
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
+    elif has_webbrowser and not without_funcs:
         """Open in default browser using webbrowser module.
         Following (currently) are the only way to suppress stdout & stderr produced by webbrowser.open().
         We suppress everything for the case if webbrowser.open() outputs something before opening.
