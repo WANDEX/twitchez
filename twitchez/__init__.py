60e18c0c24882ff89243f1a221e5384409982d17
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-02-04 17:09:11 +0300

    version: 0.0.4

--- twitchez/__init__.py
+++ twitchez/__init__.py
@@ -4,7 +4,7 @@
 from pathlib import Path
 import curses
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __license__ = "GPLv3"
 __description__ = "TUI client for twitch with thumbnails"
 __url_repository__ = "https://github.com/WANDEX/twitchez"
