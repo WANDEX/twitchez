be7f78999f21f71485dd26a5204403e6998b55f9
WANDEX <15724752+WANDEX@users.noreply.github.com>
2024-10-04 01:14:59 +0300

    [command.py] conf_cmd_check() - check, return cmd from config.

--- twitchez/command.py
+++ twitchez/command.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
+from twitchez import conf
+
 from shutil import which
 
 
-def first_cmd_word(cmd) -> str:
+def first_cmd_word(cmd: str) -> str:
     return cmd.split()[0]
 
 
-def without_funcs(executable) -> bool:
+def without_funcs(executable: str) -> bool:
     """return True if funcs are disabled via cmd in settings."""
     if "false" in executable.lower():
         return True
     return False
 
 
-def executable_check(executable) -> bool:
+def executable_check(executable: str) -> bool:
     """return True if executable(first word from cmd) set in config and found at PATH."""
-    # written this way for readability
     if executable != "undefined":
         if which(executable):
             return True
     return False
 
 
-def cmd_check(executable) -> bool:
+def cmd_check(executable: str) -> bool:
     """return True if funcs are enabled & checks of executable are passed."""
-    # written this way for readability
     if not without_funcs(executable):
         if executable_check(executable):
             return True
     return False
+
+
+def conf_cmd_check(conf_cmd: str) -> tuple[bool, str]:
+    """check, return cmd from config."""
+    cmd = conf.setting(conf_cmd)
+    executable = first_cmd_word(cmd)
+    cmd_ok = cmd_check(executable)
+    return cmd_ok, cmd
+
